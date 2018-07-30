f1bb4edd19f39bbd680a939114576d73300fc213
Mehmet Koca <mehmet.koca@commencis.com>
2018-07-30 13:44:17 +0300

    Add Commencers Model and JSON files
    Change-Id: Icb1c5699f4fed03e71cb2ebc819be94e31ab5353

---
+++ FaceDetectionApp/FaceDetectionApp/Commencer.swift
@@ -0,0 +1,14 @@
+//
+//  Commencer.swift
+//  FaceDetectionApp
+//
+//  Created by Mehmet Koca on 7/30/18.
+//  Copyright © 2018 Mehmet Koca. All rights reserved.
+//
+
+struct Commencer: Codable {
+    var name: String?
+    var title: String?
+    var department: String?
+}
+
